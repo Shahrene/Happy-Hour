f4445b499c1fc3dbd520a1338a93fa2e76ed52ba
Shahrene Davey <shahrene.davey@gmail.com>
2018-06-03 14:48:26 +1000

    completed show recipe

--- app/components/App.js
+++ app/components/App.js
@@ -11,95 +11,127 @@ export default class App extends React.Component {
     random: '',
     search: '',
     filter: '',
-    currentView: 'list',
     currDrinkIndex: null,
     showSearchInput: false,
     showFilterInput: false
+    }
   }
-}
+  inMl = (oz) => {
+    return Math.round(parseInt(oz)*29.5735)  + ' ml'
+  }
 
-handleQueryChange = (e) => {
-  this.setState({
-    search: e.target.value
-  })
-}
+  handleQueryChange = (e) => {
+    this.setState({
+      search: e.target.value
+    })
+  }
 
-handleQueryFilter = (e) => {
-  this.setState({
-    filter: e.target.value
-  })
-}
+  handleQueryFilter = (e) => {
+    this.setState({
+      filter: e.target.value
+    })
+  }
 
-handleSearch = (e) => {
-  e.preventDefault()
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php?a=Alcoholic`
-  fetch(url)
+  handleSearch = (e) => {
+    e.preventDefault()
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php?a=Alcoholic`
+    fetch(url)
+      .then(res => res.json())
+      .then(res => {
+        this.setState({
+          drinks: res.drinks
+      })
+    })
+  }
+
+  handleSearchName = (e) => {
+    e.preventDefault()
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.search}`
+    fetch(url)
     .then(res => res.json())
     .then(res => {
       this.setState({
         drinks: res.drinks
-    })
-  })
-}
-
-handleSearchName = (e) => {
-  e.preventDefault()
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.search}`
-  fetch(url)
-  .then(res => res.json())
-  .then(res => {
-    this.setState({
-      drinks: res.drinks
-  })
-})
-}
-
-handleFilterIng= (e) => {
-  e.preventDefault()
-  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.filter}`
-  fetch(url)
-  .then(res => res.json())
-  .then(res => {
-    this.setState({
-      drinks: res.drinks
-    })
-  })
-}
-
-renderSearchInput = (e) => {
-  e.preventDefault()
-  this.setState({
-    showSearchInput: !this.state.showSearchInput
-  })
-}
-
-renderFilterInput = (e) => {
-  e.preventDefault()
-  this.setState({
-    showFilterInput: !this.state.showFilterInput
-  })
-}
-
-renderDrinkItem = (drink, index) => {
-  return <div className="showDrink">
-    <h4 onClick={
-      (e) => this.setState({
-        currDrinkIndex: e.target.dataset.index
       })
-    } key={index} data-index={index}>{drink.strDrink}</h4>
-    <span> <img src={drink.strDrinkThumb} />  </span>
-    <a onClick="">Show Recipe</a>
+    })
+  }
 
-  </div>
-}
+  handleFilterIng= (e) => {
+    e.preventDefault()
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.filter}`
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({
+        drinks: res.drinks
+      })
+    })
+  }
+
+  renderSearchInput = (e) => {
+    e.preventDefault()
+    this.setState({
+      showSearchInput: !this.state.showSearchInput
+    })
+  }
+
+  renderFilterInput = (e) => {
+    e.preventDefault()
+    this.setState({
+      showFilterInput: !this.state.showFilterInput
+    })
+  }
+
+  renderDrinkItem = (drink, index) => {
+    return <div className="showDrink">
+      <h4 onClick={
+        (e) => this.setState({
+          currDrinkIndex: e.target.dataset.index
+        })
+      } key={index} data-index={index}>{drink.strDrink}</h4>
+      <span> <img src={drink.strDrinkThumb} />  </span>
+      <a onClick={this.toggleRecipe} data-index={index}>Show Recipe</a>
+      <div>{drink.showRecipe && this.renderRecipe(drink, index)}
+      </div>
+    </div>
+  }
+
+  toggleRecipe = (e) => {
+    var index = e.target.dataset.index
+    var drinks = this.state.drinks
+    drinks[index].showRecipe = !drinks[index].showRecipe
+    this.setState({ drinks })
+  }
+
+  renderRecipe = (drink, index) => {
+    return <div className="showRecipe">
+      <h4>{drink.strDrink}</h4>
+      <p>Glass: {drink.strGlass}</p>
+      <p>Method: {drink.strInstructions}</p>
+      <ul>
+        <li>{drink.strMeasure1} {drink.strIngredient1}</li>
+        <li>{drink.strMeasure2} {drink.strIngredient2}</li>
+        <li>{drink.strMeasure3} {drink.strIngredient3}</li>
+        <li>{drink.strMeasure4} {drink.strIngredient4}</li>
+        <li>{drink.strMeasure5} {drink.strIngredient5}</li>
+        <li>{drink.strMeasure6} {drink.strIngredient6}</li>
+        <li>{drink.strMeasure7} {drink.strIngredient7}</li>
+        <li>{drink.strMeasure8} {drink.strIngredient8}</li>
+        <li>{drink.strMeasure9} {drink.strIngredient9}</li>
+        <li>{drink.strMeasure10} {drink.strIngredient10}</li>
+      </ul>
+
+      <a href="#" onClick={this.state}>Back to Top</a>
+    </div>
+  }
 
   render() {
     const {drinks, currDrinkIndex, showSearchInput, showFilterInput} = this.state
 
-
     return <div className="menu">
 
       <h1>Happy Hour</h1>
+
       <form>
         <button onClick={this.handleSearch}>Random Cocktail</button>
         <button onClick={this.renderSearchInput}>Search By Cocktail Name</button>
@@ -109,7 +141,8 @@ renderDrinkItem = (drink, index) => {
               <button onClick={this.handleSearchName}>Go</button>
             </form>
           }
-        <button onClick={this.renderFilterInput}>Search By Ingredient</button>
+
+          <button onClick={this.renderFilterInput}>Search By Ingredient</button>
 
           {showFilterInput &&
             <form className="filterInput">
--- app/components/FilterBtn.js
+++ app/components/FilterBtn.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import './Recipe.scss'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-import { Router, Route, Switch } from 'react-router'
+
+
 
 export default class Recipe extends React.Component {
 
   constructor(props) {
     super(props)
     this.state = {
-    }
+      drinks: [],
+      filter: '',
+      currDrinkIndex: null,
+      showFilterInput: false
+      }
   }
 }
