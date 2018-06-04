20f8d7a841bffab2d87bee72257c792fd6e5161b
Shahrene Davey <shahrene.davey@gmail.com>
2018-06-04 17:20:40 +1000

    completed filter recipe show & loading gif

--- app/components/App.js
+++ app/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import FilterBtn from './FilterBtn'
 
 
 
@@ -7,104 +8,145 @@ export default class App extends React.Component {
   constructor(props) {
   super(props)
   this.state = {
-    drinks: [],
+    drinks:[],
     random: '',
     search: '',
     filter: '',
+    loading: false,
+    currentRecipe: null,
     currDrinkIndex: null,
     showSearchInput: false,
-    showFilterInput: false
+    clicked: true
     }
   }
-  inMl = (oz) => {
-    return Math.round(parseInt(oz)*29.5735)  + ' ml'
-  }
-
+//for search input box
   handleQueryChange = (e) => {
     this.setState({
+      drinks: [],
       search: e.target.value
     })
   }
-
+//for filter input box
   handleQueryFilter = (e) => {
     this.setState({
+      drinks: [],
       filter: e.target.value
     })
   }
-
+//call for random search
   handleSearch = (e) => {
     e.preventDefault()
-    const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php?a=Alcoholic`
+    this.setState({loading: true})
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/random.php?`
     fetch(url)
       .then(res => res.json())
       .then(res => {
         this.setState({
+          loading: false,
           drinks: res.drinks
       })
     })
   }
-
+//call for search for cocktail by name
   handleSearchName = (e) => {
     e.preventDefault()
+    this.setState({loading: true})
     const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${this.state.search}`
     fetch(url)
     .then(res => res.json())
     .then(res => {
       this.setState({
+        loading: false,
         drinks: res.drinks
       })
     })
   }
-
+//call for filtering by ingredient
   handleFilterIng= (e) => {
     e.preventDefault()
+    this.setState({loading: true})
     const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${this.state.filter}`
     fetch(url)
     .then(res => res.json())
     .then(res => {
       this.setState({
-        drinks: res.drinks
+        loading: false,
+        drinks: res.drinks.map(d => ({ ...d, showing: false }))
       })
     })
   }
-
+// input box to show when button pressed
   renderSearchInput = (e) => {
     e.preventDefault()
     this.setState({
-      showSearchInput: !this.state.showSearchInput
+      showSearchInput: !this.state.showSearchInput,
+      drinks: []
     })
   }
-
-  renderFilterInput = (e) => {
-    e.preventDefault()
-    this.setState({
-      showFilterInput: !this.state.showFilterInput
-    })
-  }
-
+//renders the search results for all three calls
   renderDrinkItem = (drink, index) => {
-    return <div className="showDrink">
+    return <div key={index} className="showDrink">
       <h4 onClick={
         (e) => this.setState({
           currDrinkIndex: e.target.dataset.index
         })
       } key={index} data-index={index}>{drink.strDrink}</h4>
+
       <span> <img src={drink.strDrinkThumb} />  </span>
-      <a onClick={this.toggleRecipe} data-index={index}>Show Recipe</a>
-      <div>{drink.showRecipe && this.renderRecipe(drink, index)}
+      <a onClick={() => this.showRecipe(drink)} data-index={index}>"Show Recipe"</a>
+      {/*   */}
+
+      <div>{drink.fetched && this.renderRecipe(drink, index)}
       </div>
     </div>
   }
+//shows the recipe when user clicks on 'show recipe'
+  showRecipe = (drink) => {
 
-  toggleRecipe = (e) => {
-    var index = e.target.dataset.index
-    var drinks = this.state.drinks
-    drinks[index].showRecipe = !drinks[index].showRecipe
-    this.setState({ drinks })
+    // this.setState(prev => (
+    //   {
+    //     drinks: prev.drinks.map(d =>
+    //       d.idDrink === drink.idDrink ?
+    //         { ...d, showing: !d.showing } :
+    //         d
+    //       )
+    //   }
+    // ))
+    // var index = e.target.dataset.index
+    // var drinks = this.state.drinks
+
+
+    // this.setState({clicked: !this.state.clicked})
+    // drinks[index].showRecipe = !drinks[index].showRecipe
+    // this.setState({ drinks })
+    // console.log(drinks[index].idDrink)
+
+    // makes further call for filter search to fetch drink by id
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.idDrink}`)
+      .then(res => res.json())
+      .then(res => {
+
+        const newDrink = res.drinks[0]
+        newDrink.fetched = true
+
+        this.setState(prev => (
+          {
+            drinks: prev.drinks.map(d => {
+              if (d.idDrink === newDrink.idDrink) {
+                return newDrink
+              } else {
+                return d
+              }
+            })
+          }
+        ))
+      })
   }
+
 
   renderRecipe = (drink, index) => {
     return <div className="showRecipe">
+
       <h4>{drink.strDrink}</h4>
       <p>Glass: {drink.strGlass}</p>
       <p>Method: {drink.strInstructions}</p>
@@ -121,7 +163,8 @@ export default class App extends React.Component {
         <li>{drink.strMeasure10} {drink.strIngredient10}</li>
       </ul>
 
-      <a href="#" onClick={this.state}>Back to Top</a>
+      <a href="#">Back to Top</a>
+
     </div>
   }
 
@@ -135,23 +178,19 @@ export default class App extends React.Component {
       <form>
         <button onClick={this.handleSearch}>Random Cocktail</button>
         <button onClick={this.renderSearchInput}>Search By Cocktail Name</button>
-          {showSearchInput &&
-            <form className="searchInput">
-              <input type="text" onChange={this.handleQueryChange} />
-              <button onClick={this.handleSearchName}>Go</button>
-            </form>
-          }
-
-          <button onClick={this.renderFilterInput}>Search By Ingredient</button>
-
-          {showFilterInput &&
-            <form className="filterInput">
-              <input type="text" onChange={this.handleQueryFilter} />
-              <button onClick={this.handleFilterIng}>Go</button>
-            </form>
-          }
       </form>
+
+      {showSearchInput &&
+        <form className="searchInput">
+          <input type="text" onChange={this.handleQueryChange} />
+          <button onClick={this.handleSearchName}>Go</button>
+        </form>
+      }
+
+
+      <FilterBtn filter={this.handleFilterIng} handleChange={this.handleQueryFilter} />
       <hr />
+      {this.state.loading && <img className="gif" src="https://media.giphy.com/media/3oEduYlHXf2yfop1HW/giphy.gif" />}
       {drinks.map(this.renderDrinkItem)}
 
       </div>
